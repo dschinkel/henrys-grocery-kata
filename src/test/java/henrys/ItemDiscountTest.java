53fa51d6c5cea51b128bc9a362170d8616f63936
Dave Schinkel <dschinkel@gmail.com>
2021-02-07 21:19:23 -0600

    Refactor tests

--- src/test/java/henrys/ItemDiscountTest.java
+++ src/test/java/henrys/ItemDiscountTest.java
@@ -32,18 +32,18 @@ public class ItemDiscountTest {
     double baseTotalPrice = 3.55;
     LocalDate dateTwoDaysAgo = LocalDate.now().minusDays(2);
 
-    double discountedPrice = itemDiscount.twoSoupGetOneLoafBreadHalfOff(purchasedItems, baseTotalPrice, dateTwoDaysAgo);
+    double nonDiscountedPrice = itemDiscount.twoSoupGetOneLoafBreadHalfOff(purchasedItems, baseTotalPrice, dateTwoDaysAgo);
 
-    assertEquals(baseTotalPrice, discountedPrice);
+    assertEquals(baseTotalPrice, nonDiscountedPrice);
   }
 
   @Test
-  void threeSoup_twoLoavesBread_purchasedDateToday() {
+  void threeSoup_twoLoavesBread_withDiscount_purchasedDateToday() {
     addToPurchasedItems(SOUP, 3);
     addToPurchasedItems(BREAD, 2);
     double baseTotalPrice = 3.55;
-    LocalDate dateTwoDaysAgo = LocalDate.now();
     double expectdDiscountedPrice = 3.15;
+    LocalDate dateTwoDaysAgo = LocalDate.now();
 
     double discountedPrice = itemDiscount.twoSoupGetOneLoafBreadHalfOff(purchasedItems, baseTotalPrice, dateTwoDaysAgo);
 
@@ -51,19 +51,20 @@ public class ItemDiscountTest {
   }
 
   @Test
-  void sixApples_and_oneBottleOfMilk_purchasedThreeDaysFromToday() {
-    addToPurchasedItems(APPLE, 6);
-    addToPurchasedItems(MILK, 1);
-    double baseTotalPrice = 1.84;
-    LocalDate dateToday = LocalDate.now().plusDays(5);
+  void threeSoup_twoLoavesBread_withDiscount_purchasedYesterday() {
+    addToPurchasedItems(SOUP, 3);
+    addToPurchasedItems(BREAD, 2);
+    double baseTotalPrice = 3.55;
+    double expectdDiscountedPrice = 3.15;
+    LocalDate dateTwoDaysAgo = LocalDate.now().minusDays(1);
 
-    double discountedPrice = itemDiscount.twoSoupGetOneLoafBreadHalfOff(purchasedItems, baseTotalPrice, dateToday);
+    double discountedPrice = itemDiscount.twoSoupGetOneLoafBreadHalfOff(purchasedItems, baseTotalPrice, dateTwoDaysAgo);
 
-    assertEquals(baseTotalPrice, discountedPrice);
+    assertEquals(expectdDiscountedPrice, discountedPrice);
   }
 
   @Test
-  void apples_purchased_today() {
+  void apples_noDiscount_purchasedToday() {
     addToPurchasedItems(APPLE, 3);
     LocalDate dateToday = LocalDate.now();
     double baseTotalPrice = .30;
@@ -75,7 +76,7 @@ public class ItemDiscountTest {
   }
 
   @Test
-  void apples_purchased_threeDaysFromToday() {
+  void threeApples_withDiscount_purchasedThreeDaysFromToday() {
     addToPurchasedItems(APPLE, 3);
     LocalDate dateThreeDaysFromToday = LocalDate.now().plusDays(3);
     double baseTotalPrice = .30;
@@ -86,21 +87,46 @@ public class ItemDiscountTest {
     assertEquals(expectedDiscountedPrice, discountedPrice);
   }
 
+  @Test
+  void sixApples_and_oneBottleOfMilk_noDiscount_purchasedToday() {
+    addToPurchasedItems(APPLE, 6);
+    addToPurchasedItems(MILK, 1);
+    double baseTotalPriceWithoutDiscount = 1.90;
+    LocalDate dateToday = LocalDate.now();
+
+    double discountedPrice = itemDiscount.applyAppleTenPercentDiscount(purchasedItems, baseTotalPriceWithoutDiscount, dateToday);
+
+    assertEquals(baseTotalPriceWithoutDiscount, discountedPrice);
+  }
+
+  @Test
+  void sixApples_and_oneBottleOfMilk_withDiscount_purchasedWithinThreeDaysFromToday() {
+    addToPurchasedItems(APPLE, 6);
+    addToPurchasedItems(MILK, 1);
+    double baseTotalPrice = 1.90;
+    double expectedDiscountedPrice = 1.84;
+    LocalDate dateToday = LocalDate.now().plusDays(3);
+
+    double discountedPrice = itemDiscount.applyAppleTenPercentDiscount(purchasedItems, baseTotalPrice, dateToday);
+
+    assertEquals(expectedDiscountedPrice, discountedPrice);
+  }
+
+  @Test
+  void threeApples_noDiscount_purchasedAfterEndOfFollowingMonth() {
+    addToPurchasedItems(APPLE, 3);
+    double baseTotalPrice = .30;
+    LocalDate afterEndOfNextMonth = LocalDate.now().plusMonths(1).plusDays(1);
+
+    double nonDiscountedPrice = itemDiscount.applyAppleTenPercentDiscount(purchasedItems, baseTotalPrice, afterEndOfNextMonth);
+
+    assertEquals(baseTotalPrice, nonDiscountedPrice);
+  }
+
   private void addToPurchasedItems(Constants.ItemName itemName, Integer howManyToAdd) {
     StockItem item = stockItemsDB.get(itemName.getValue());
     for (int i = 0; i < howManyToAdd; i++) {
       purchasedItems.add(item);
     }
-  }
-
-  @Test
-  void oneApple__purchasedDate_noDiscount_whenDateAfterEndOfFollowingMonth() {
-    addToPurchasedItems(APPLE, 3);
-    double baseTotalPrice = .20;
-    LocalDate afterEndOfNextMonth = LocalDate.now().plusMonths(1).plusDays(1);
-
-    double discountedPrice = itemDiscount.applyAppleTenPercentDiscount(purchasedItems, baseTotalPrice, afterEndOfNextMonth);
-
-    assertEquals(baseTotalPrice, discountedPrice);
   }
 }
